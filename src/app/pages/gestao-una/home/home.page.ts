e532dde9c1f2e2b6e81a7d5fb05e6bac21ee2f19
RenataIART <renataaugustomaia@gmail.com>
2026-08-07 17:35:44 -0300

    feat: add footer to Gestao UNA home page

--- src/app/pages/gestao-una/home/home.page.ts
+++ src/app/pages/gestao-una/home/home.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FooterComponent } from '../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
